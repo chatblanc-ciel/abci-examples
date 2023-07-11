f3d965125bc39c9fcb49d6ac4fd61b86a730754c
chatblanc-ciel <matsumoto9845@spice.ocn.ne.jp>
2023-07-12 04:26:49 +0900

    Add full finetuning with deepspeed script

---
+++ 202307/scripts/finetune_deepspeed.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -l rt_G.large=1
+#$ -l h_rt=4:00:00
+#$ -j y
+#$ -N finetune-deepspeed
+#$ -o logs/
+#$ -cwd
+
+source /etc/profile.d/modules.sh
+module load cuda/11.8 cudnn/8.9
+source .venv/bin/activate
+
+export HF_HOME=/scratch/$(whoami)/.cache/huggingface/
+export SCRATCH_HOME=/scratch/$(whoami)
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+# set the wandb project where this run will be logged
+export WANDB_PROJECT="rinna"
+# save your trained model checkpoint to wandb
+export WANDB_LOG_MODEL="true"
+# turn off watch to log faster
+export WANDB_WATCH="false"
+
+wandb login $WANDB_KEY
+deepspeed --num_gpus=$GPUS src/finetune.py --model_name $MODEL --config_file $CONFIG
